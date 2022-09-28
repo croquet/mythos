310898bb752963a00d7426547fe44218fb368724
Croqueteer <davidasmith@gmail.com>
2022-09-28 11:17:32 -0700

    Fix appId

--- apiKey.js
+++ apiKey.js
@@ -1,4 +1,4 @@
 /* Copy this into a file named apiKey.js */
 const apiKey = '1DpF4lnxxGr0IjKbKlOm58A9GuKbFyKaxh2PDK87x';
-const appId = 'io.croquet.physics';
+const appId = 'io.croquet.mythos';
 export default {apiKey, appId};
