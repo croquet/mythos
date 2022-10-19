649bbd1a8565cb2f478d543a1967440d0b08ae21
Yoshiki Ohshima <yoshiki.ohshima@acm.org>
2022-10-18 17:53:58 -0700

    make sure that you can go into water

--- behaviors/croquet/halfBodyAvatar.js
+++ behaviors/croquet/halfBodyAvatar.js
@@ -27,6 +27,9 @@ class AvatarPawn {
 
         if (!this.isMyPlayerPawn) {return;}
 
+        this.maxFall = -100;
+        this.fallDistance = this.eyeHeight / 12;
+
         this.addFirstResponder("pointerTap", {ctrlKey: true, altKey: true}, this);
         this.addEventListener("pointerTap", this.pointerTap);
 
